4909c11d25787b1c211e51a791cf141553c37e32
vitalis <vitalismutwiri2gmail.com>
2022-05-31 22:46:09 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='imgs' src="/images/bg-pattern-top-desktop.svg" alt="topbackground" />
+      <img  className='imgs1' src="/images/bg-pattern-bottom-desktop.svg" alt="backgroundbottom" />
+      <div className='container' >
+        
+      </div>
     </div>
   );
 }
